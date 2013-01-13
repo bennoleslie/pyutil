805ba9d93e04ed3991d8d00132e439df02eaff79
Benno Leslie <benno@breakawayconsulting.com.au>
2013-01-14 09:09:06 +1100

    Add import_from_dir() function

--- util.py
+++ util.py
@@ -30,3 +30,33 @@ class Locator:
         line = bisect.bisect_right(self.line_offsets, offset)
         col = offset - self.line_offsets[line - 1]
         return Location(self.name, line, col)
+
+
+def import_from_dir(module_name, dir_name):
+    """Import a module form a specific directory.
+
+    Sometimes you might want to load a package from a specific
+    directory. For example, you may be loading a plugin of some
+    description.
+
+    This function ensures that only modules from a specific
+    directory are loaded to avoid any chance of loading a
+    module of the same name from somewhere else.
+
+    After loading the module is removed from sys.modules to
+    avoid other namespace clashes.
+
+    """
+    saved_sys_path = sys.path
+    saved_module = None
+    if module_name in sys.modules:
+        saved_module = sys.modules[module_name]
+    try:
+        module = __import__(module_name)
+        return module
+    finally:
+        sys.path = saved_sys_path
+        if saved_module:
+            sys.modules[module_name] = saved_module
+        else:
+            del sys.modules[module_name]
